290aa1f5829b3d03a21f669f86d6f95164961695
Jens Mittag <jens.mittag@gmail.com>
2013-11-27 17:34:37 +0100

    Updated copyright information in test case files

--- sources/src/stdma/test/single-node-test.h
+++ sources/src/stdma/test/single-node-test.h
@@ -1,6 +1,6 @@
 /* -*-  Mode: C++; c-file-style: "gnu"; indent-tabs-mode:nil; -*- */
 /*
- * Copyright (c) 2013 Jens Mittag
+ * Copyright (c) 2013 Jens Mittag, Tristan Gaugel
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
@@ -15,8 +15,8 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * Author: Jens Mittag <jens.mittag@kit.edu>
- *
+ * Author: Jens Mittag <jens.mittag@gmail.com>
+ *         Tristan Gaugel <tristan.gaugel@kit.edu>
  */
 
 #ifndef SINGLE_NODE_TEST_H_
